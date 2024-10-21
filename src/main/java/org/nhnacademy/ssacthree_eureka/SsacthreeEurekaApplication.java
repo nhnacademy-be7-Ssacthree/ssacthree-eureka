1ded9a6e6d8f76fee812141d1bc9d374a9cef28f
minji <qkralswl6125@naver.com>
2024-10-21 15:35:16 +0900

    init

---
+++ src/main/java/org/nhnacademy/ssacthree_eureka/SsacthreeEurekaApplication.java
@@ -0,0 +1,13 @@
+package org.nhnacademy.ssacthree_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsacthreeEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsacthreeEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nhnacademy/ssacthree_eureka/SsacthreeEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nhnacademy.ssacthree_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsacthreeEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
